b71f8d7820d2a087244c96ebde2e8e4fd0dc5a7d
fkowalcz <fryderyk.kowalcz@gmail.com>
2024-06-19 02:21:40 +0200

    only ball recall missing

---
+++ translations/translations_en.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>MainWindow-en</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="71"/>
+        <source>Plot T axis -</source>
+        <translation>Plot T axis -</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="78"/>
+        <source>Plot T axis +</source>
+        <translation>Plot T axis +</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="87"/>
+        <source>Start</source>
+        <translation>Start</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="106"/>
+        <source>Width +</source>
+        <translation>Width +</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="113"/>
+        <source>Width -</source>
+        <translation>Width -</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/translations_pl.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished">MainWindow-pl</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="71"/>
+        <source>Plot T axis -</source>
+        <translation type="unfinished">Oś T -</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="78"/>
+        <source>Plot T axis +</source>
+        <translation type="unfinished">Oś T +</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="87"/>
+        <source>Start</source>
+        <translation type="unfinished">Odpalaj</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="106"/>
+        <source>Width +</source>
+        <translation type="unfinished">Szer. belki +</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="113"/>
+        <source>Width -</source>
+        <translation type="unfinished">Szer. belki -</translation>
+    </message>
+</context>
+</TS>
